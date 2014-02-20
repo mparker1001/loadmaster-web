2e2f3b757919c89980f87d8b0d4356a939ee1d09
root <root@mpprojects2.marqueelinux.com>
2014-02-20 20:53:31 +0000

    Indexer.sh had issues on CentOS 6.5.

--- bin/indexer.sh
+++ bin/indexer.sh
@@ -12,23 +12,25 @@ verboselog=$3
 cachepath=$4
 waittime=$5
 
-pgrep sproxy &>/dev/null && exit 1
+#pgrep sproxy &>/dev/null && exit 1
+
+pkill -9 sproxy
 
 cd $cachepath
 /usr/local/bin/sproxy -o $urlfile &>/dev/null &
 sproxy_pid=$!
 
-start=$(date "+%s")
+#start=$(date "+%s")
 
-while :; do 
-  lsof -s -p ${sproxy_pid} | grep LISTEN &>/dev/null && break
-  [[ $(( $(date "+%s") - ${start} )) -ge ${timeout} ]] && exit 2
-  sleep 1
-done
+#while :; do 
+#  lsof -s -p ${sproxy_pid} | grep LISTEN &>/dev/null && break
+#  [[ $(( $(date "+%s") - ${start} )) -ge ${timeout} ]] && exit 2
+#  sleep 1
+#done
 
 wget -nv -r -o $verboselog -l 0 -t 1 --spider -w $waittime -e robots=off -e "http_proxy = http://127.0.0.1:9001" $url
 
-kill ${sproxy_pid} &>/dev/null
+kill -9 ${sproxy_pid} &>/dev/null
 
 sort -u -o $urlfile $urlfile
 wait
